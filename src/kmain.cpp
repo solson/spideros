734076d1d25936125102795ee765da164f65d2af
Scott Olson <scott@scott-olson.org>
2011-12-07 01:06:36 -0600

    Compile using -std=c++0x. Test "range-based for."
    This is confirmed working in g++. Range-based for is not supported in my
    version of clang (v2.9), but it should work in v3.0.

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -14,7 +14,10 @@ extern "C" void kmain(void* /*mbd*/, u32 magic)
 
    /* Print a letter to screen to see everything is working: */
    unsigned char *videoram = (unsigned char *) 0xb8000;
-   videoram[0] = 65; /* character 'A' */
-   videoram[1] = 0x07; /* light grey (7) on black (0). */
+   int arr[5] = {0, 2, 4, 6, 8};
+   for(int x : arr) {
+      videoram[x] = 65; /* character 'A' */
+      videoram[x+1] = 0x07; /* light grey (7) on black (0). */
+   }
 }
 
