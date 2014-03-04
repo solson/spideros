4e11c38570373c56f1fc4c299053e2aa3b9fd766
Scott Olson <scott@scott-olson.org>
2014-03-04 00:03:04 -0600

    Remove old test.

--- src/kmain.cpp
+++ src/kmain.cpp
@@ -3,14 +3,6 @@
 #include "multiboot.h"
 #include "gdt.h"
 
-struct TestDestructors {
-    const char* str;
-    TestDestructors(const char* str_) : str(str_) {}
-    ~TestDestructors() { display::println(str, " destructed"); }
-};
-
-TestDestructors bar("bar");
-
 extern "C" void kmain(multiboot::Info* mbinfo, u32 magic) {
     display::init();
 
@@ -29,8 +21,7 @@ extern "C" void kmain(multiboot::Info* mbinfo, u32 magic) {
     display::println("This is", " a", ' ', 't', "est", '!');
     const int a = 12, b = 3;
     display::println("Hey look, numbers: ", a, " * ", b, " = ", a * b);
-
-    display::println(); // Just a newline.
+    display::println();
 
     if(mbinfo->hasFlag(multiboot::BOOTLOADER_NAME))
         display::println("Bootloader:\t", (const char*) mbinfo->bootloaderName);
@@ -38,15 +29,14 @@ extern "C" void kmain(multiboot::Info* mbinfo, u32 magic) {
     if(mbinfo->hasFlag(multiboot::COMMAND_LINE))
         display::println("Command line:\t", (const char*) mbinfo->commandLine);
 
+    display::println();
     display::println("Hex: 0x", display::hex(120), '\n',
                      "Oct: 0",  display::oct(120), '\n',
                      "Bin: 0b", display::bin(120), '\n',
                      "Dec: ", 120);
+    display::println();
 
-    display::println("Initializing GDT...");
+    display::print("Initializing GDT... ");
     gdt::init();
-
-    TestDestructors foo("foo");
-    TestDestructors baz("baz");
+    display::print("done.");
 }
-
